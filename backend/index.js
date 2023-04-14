c42112ecc3e7eb1632dbfe725bc83abbabb5962f
Doniyor Kuzibaev <doniyor.kuzibaev@yandex.ru>
2023-04-14 14:28:33 +0300

    finished

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, fist_name: username },
+      { headers: { "private-key": "d4ed2e51-ae6f-40c6-947d-8be40df94230" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
